ead92da22e0e998a2f36e2332799a9371f56e4b9
DulanjanaMaduwantha <dulanjanamaduwantha@gmail.com>
2024-04-19 13:14:34 +0530

    structure changed

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Welcome to MERN
+        </h1>
+        <button className="users-button">User Button</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
