7d8915a7b8243deac158ac1224188cd55c3ab722
DulanjanaMaduwantha <dulanjanamaduwantha@gmail.com>
2024-04-20 23:33:10 +0530

    mongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,32 @@
-const app=require('./app');
+const express = require('express');
+const app= express();
+const cors =require('cors');
 const port=3001;
-const host='127.0.0.1';
+const host='localhost';
+const mongoose=require('mongoose');
+const router=require('./router');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://sirimath93:sirimath%40123@cluster0.r8diuku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+
+
+const connect= async ()=>{
+    try{
+        await mongoose.connect(uri);
+        console.log('connected to mongoDB');
+    }
+    catch(error){
+        console.log('mongo error: ' , error);
+    }
+};
+
+connect();
 
 const server = app.listen(3001,'127.0.0.1',()=>{
     console.log(`server is running on port ${server.address().port}`);
 });
+
+app.use('/api',router);
